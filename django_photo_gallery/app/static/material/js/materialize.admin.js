1c527dc19d96106eaedc74620c36bcfdb3e9f42c
Velin Georgiev <velin.georgiev@gmail.com>
2018-04-06 23:06:22 +0100

    Removing django-material

--- django_photo_gallery/app/static/material/js/materialize.admin.js
+++
@@ -1,4 +0,0 @@
-$(document).on('ready', function() {
-  $('.button-collapse').sideNav()
-  $('#slide-out').perfectScrollbar()
-});
--- django_photo_gallery/django_photo_gallery/urls.py
+++ django_photo_gallery/django_photo_gallery/urls.py
@@ -7,29 +7,19 @@ from django.conf.urls.static import static
 
 import app.forms
 import app.views
-
-from django.conf.urls import include
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', app.views.gallery, name='gallery'),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/icons/favicon.ico', permanent=True)),
-    url(r'^(?P<slug>[-\w]+)$', app.views.AlbumDetail.as_view(), name='album'), #app.views.AlbumView.as_view()
+    url(r'^(?P<slug>[-\w]+)$', app.views.AlbumDetail.as_view(), name='album'),
      
-    # Auth related urls
+    # admin
     url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^logout$', auth_views.logout, { 'next_page': '/', }, name='logout'),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', admin.site.urls),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-handler404 = 'app.views.handler404'
-
 
 
 
